eb1e61c62bd9d5e4b0bf4d279676b9a8199ab734
ondrej gabco <ondrej.gabco@qats.sk>
2024-02-04 21:37:03 +0100

    transfer to intellij IDE

--- 101daysOfCode/trialRun/pyPasswordGenerator.py
+++ 101daysOfCode/trialRun/pyPasswordGenerator.py
@@ -2,9 +2,9 @@ import os
 import random
 
 file_name = os.path.basename(__file__)
-print(file_name) # Output: <name of the current script file>
+print(file_name)  # Output: <name of the current script file>
 
-print("\n---------- Start of program " + file_name + "-----------\n") 
+print("\n---------- Start of program " + file_name + "-----------\n")
 
 n_letters = int(input("How many letters would you like to have in your password? "))
 # ascii 65-90 for capital letters and 97-122 for small letters
@@ -13,39 +13,35 @@ n_numbers = int(input("How many numbers would you like to have in your password?
 n_symbols = int(input("How many symbols would you like to have in your password? "))
 # ascii 33-47 for symbols
 
-settings = [[n_letters,"letters"],[n_numbers,"numbers"],[n_symbols,"symbols"]]
+settings = [[n_letters, "letters"], [n_numbers, "numbers"], [n_symbols, "symbols"]]
 
-#print(settings)
+# print(settings)
 
-for j in range(0,3):
-    for i, set in enumerate(settings):
-        if (set[0]==0):
+for j in range(0, 3):
+    for i, setting in enumerate(settings):
+        if setting[0] == 0:
             settings.pop(i)
 
-
-total_chars = n_letters+n_numbers+n_symbols
+total_chars = n_letters + n_numbers + n_symbols
 password = ""
-for i in range(0,total_chars):
+for i in range(0, total_chars):
     if settings:
-        rand_index = random.randint(0,len(settings)-1)
+        rand_index = random.randint(0, len(settings) - 1)
         character = settings[rand_index][1]
-        if (character == "letters"):
-            if (random.randint(0,1)==0):
-                password+=chr(random.randint(65,90))
+        if character == "letters":
+            if random.randint(0, 1) == 0:
+                password += chr(random.randint(65, 90))
             else:
-                password+=chr(random.randint(97,122))
-        elif (character == "numbers"):
-            password+=chr(random.randint(48,57))
+                password += chr(random.randint(97, 122))
+        elif character == "numbers":
+            password += chr(random.randint(48, 57))
         else:
-            password+=chr(random.randint(33,47))
-        if (settings[rand_index][0]==1):
+            password += chr(random.randint(33, 47))
+        if settings[rand_index][0] == 1:
             settings.pop(rand_index)
         else:
-            settings[rand_index][0]-=1
-        
+            settings[rand_index][0] -= 1
 
 print(f"\nHere is your password: {password}")
 
-print("\n------------ End of program " + file_name + "-----------\n") 
-
-
+print("\n------------ End of program " + file_name + "-----------\n")
