9428dd6235d1ead11198f48ad249eb95bb21792e
ondrej gabco <ondrej.gabco@qats.sk>
2024-05-05 21:40:56 +0200

    day25 guessing game final

--- 101daysOfCode/us_states_game/us_states_game_main.py
+++ 101daysOfCode/us_states_game/us_states_game_main.py
@@ -2,9 +2,14 @@ from turtle import Turtle, Screen
 import pandas
 import turtle
 
-
 IMAGE = "blank_states_img.gif"
 DATA_FILE = "50_states.csv"
+RESULT_FILE = "missed_states.txt"
+
+
+def missing_states(all_states, guessed_states):
+    return list(set(all_states) - set(guessed_states))
+
 
 screen = Screen()
 screen.setup(width=725, height=491)
@@ -14,23 +19,35 @@ turtle.shape(IMAGE)
 
 states_data = pandas.read_csv(DATA_FILE)
 score = 0
-
+guessed_states_list = []
 
 while True:
     title = "Guess the State"
     prompt = "Type the name of the state that is not revealed yet"
-    answer_state = screen.textinput(title=f"{title} | Current score: {score}/50", prompt=prompt)
-    answer_state_data = states_data[states_data.state == answer_state]
-    if not answer_state_data.empty:
-        answer_list = answer_state_data.to_dict('list')
-        state_name = Turtle()
-        state_name.hideturtle()
-        state_name.penup()
-        state_name.setpos(answer_list['x'][0], answer_list['y'][0])
-        state_name.write(arg=answer_list['state'][0], align="center", font=("Courier", 8, "normal"))
-        score += 1
-    else:
+    answer_state = screen.textinput(title=f"{title} | Current score: {score}/50", prompt=prompt).title()
+    if answer_state == 'Exit':
+        with open(RESULT_FILE, "w") as file:
+            for item in missing_states(states_data.state.to_list(), guessed_states_list):
+                file.write(str(item) + '\n')
         break
-
-
+    answer_state_data = states_data[states_data.state == answer_state]
+    # split = states_data[states_data.state == answer_state].values[0]
+    # print(split[0])
+    if not answer_state_data.empty:
+        state = answer_state_data.state.item()
+        x_cor = answer_state_data.x.item()
+        y_cor = answer_state_data.y.item()
+        if state not in guessed_states_list:
+            guessed_states_list.append(state)
+            state_name = Turtle()
+            state_name.hideturtle()
+            state_name.penup()
+            state_name.setpos(x_cor, y_cor)
+            state_name.write(arg=state, align="center", font=("Courier", 8, "normal"))
+            score += 1
+            if score == 50:
+                print("Congrats, you've won!!!")
+                break
+            # print(state)
+print("Bye bye")
 turtle.mainloop()
