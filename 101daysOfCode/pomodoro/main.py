bbffdc60dde9ce3999a373aca744871322012799
ondrej gabco <ondrej.gabco@qats.sk>
2024-06-09 22:27:29 +0200

    final

--- 101daysOfCode/pomodoro/main.py
+++ 101daysOfCode/pomodoro/main.py
@@ -9,14 +9,61 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+SECONDS_IN_MINUTE = 60
+reps = 0
+main_timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def do_reset():
-    pass
+    global reps
+    global main_timer
+
+    lbl_check.configure(text="")
+    lbl_heading.configure(text="Timer", fg=GREEN)
+    reps = 0
+    window.after_cancel(main_timer)
+    cnv_canvas.itemconfig(timer_text, text="00:00")
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def do_start():
-    pass
+    global reps
+
+    reps += 1
+    if reps % 2 == 1:
+        count_down(WORK_MIN * SECONDS_IN_MINUTE)
+        lbl_heading.configure(text="Work", fg=GREEN)
+    elif reps == 8:
+        count_down(LONG_BREAK_MIN * SECONDS_IN_MINUTE)
+        lbl_heading.configure(text="Rest", fg=RED)
+    else:
+        count_down(SHORT_BREAK_MIN * SECONDS_IN_MINUTE)
+        lbl_heading.configure(text="Rest", fg=PINK)
+    marks = ""
+    for i in range(int(reps / 2)):
+        marks += "✔"
+    lbl_check.configure(text=marks)
+    if reps == 8:
+        reps = 0
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global main_timer
+    minutes = int(count / 60)
+    if minutes < 10:
+        minutes = f"0{minutes}"
+    seconds = int(count % 60)
+    if seconds < 10:
+        seconds = f"0{seconds}"
+    timer_digits = f"{minutes}:{seconds}"
+    cnv_canvas.itemconfig(timer_text, text=timer_digits)
+    if count > 0:
+        main_timer = window.after(1000, count_down, count - 1)
+    else:
+        do_start()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -29,7 +76,7 @@ lbl_heading.grid(row=0, column=1)
 cnv_canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 cnv_canvas.create_image(100, 112, image=tomato_img)
-cnv_canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = cnv_canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 cnv_canvas.grid(row=1, column=1)
 
 cmd_start = Button(text="Start", command=do_start)
@@ -38,7 +85,7 @@ cmd_start.grid(row=2, column=0)
 cmd_reset = Button(text="Reset", command=do_reset)
 cmd_reset.grid(row=2, column=2)
 
-lbl_check = Label(text="✔", fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
+lbl_check = Label(fg=GREEN, font=(FONT_NAME, 35, "bold"), bg=YELLOW)
 lbl_check.grid(row=3, column=1)
 
 window.mainloop()
